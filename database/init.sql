5c4d40642edc4cf24954107776bd10267b04cb4d
Niels Evant Robort <41827183+EnchantedPupu@users.noreply.github.com>
2024-12-01 18:00:06 +0800

    Add files via upload

--- database/init.sql
+++ database/init.sql
@@ -3,11 +3,46 @@ use elounge;
 
 create table users (
     id int primary key auto_increment,
-    username varchar(255) not null,
-    password varchar(255) not null,
+    name varchar(255) not null,
     email varchar(255) not null,
+    password varchar(255) not null,
+    isadmin int default 0,
+    sex varchar(255) not null,
+    profile_pic varchar(255) not null,
+    created_at timestamp default current_timestamp
+);
+
+create table rooms (
+    id int primary key auto_increment,
+    name varchar(255) not null,
+    block varchar(255) not null,
+    unit varchar(255) not null,
+    type varchar(255) not null,
+    status varchar(255) not null,
+    max_persons int not null,
+    created_at timestamp default current_timestamp
+);
+
+create table bookings (
+    id int primary key auto_increment,
+    user_id int not null,
+    room_id int not null,
+    date date not null,
+    approval varchar(255) not null,
+    check_in date not null,
+    check_out date not null,
+    total_person int not null,
+    created_at timestamp default current_timestamp
+);
+
+create table transactions (
+    id int primary key auto_increment,
+    booking_id int not null,
+    amount int not null,
+    transactions_type varchar(255) not null,
+    status varchar(255) not null,
     created_at timestamp default current_timestamp
 );
 
 create user 'elounge'@'localhost' identified by '123456';
-grant select, insert, update on * to 'elounge'@'localhost';
+grant select, insert, update, delete on * to 'elounge'@'localhost';
